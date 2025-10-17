d7d69830e7e5c1b4b47e3c00cfc6f41827a585c8
dmakarau <denis.makarow@gmail.com>
2025-10-17 17:35:11 +0200

    Initial Commit

---
+++ CryptoCurrenciesApp/CryptoCurrenciesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrenciesAppApp.swift
+//  CryptoCurrenciesApp
+//
+//  Created by Denis Makarau on 17.10.25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrenciesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
